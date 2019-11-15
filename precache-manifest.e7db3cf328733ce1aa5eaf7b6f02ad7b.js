a1c5302687b09890bdd9f38044292af666edeeb9
Kallemakela <kalle.makela@aalto.fi>
2019-11-15 12:34:14 +0200

    Updates

---
+++ precache-manifest.e7db3cf328733ce1aa5eaf7b6f02ad7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3062a5b6519abf80cae1018dfeaba6be",
+    "url": "/CourseRS/index.html"
+  },
+  {
+    "revision": "4d3494d826b860109355",
+    "url": "/CourseRS/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d50dc6ad5d0b1f3cbeff",
+    "url": "/CourseRS/static/css/main.ca32df67.chunk.css"
+  },
+  {
+    "revision": "4d3494d826b860109355",
+    "url": "/CourseRS/static/js/2.df94609b.chunk.js"
+  },
+  {
+    "revision": "d50dc6ad5d0b1f3cbeff",
+    "url": "/CourseRS/static/js/main.75508273.chunk.js"
+  },
+  {
+    "revision": "ffce89abbff8344fb68f",
+    "url": "/CourseRS/static/js/runtime-main.9ea1e272.js"
+  }
+]);
