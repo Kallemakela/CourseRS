7861e30331d5cfff176b0ef49a757b3063758bd5
Kallemakela <kalle.makela@aalto.fi>
2019-11-18 10:54:15 +0200

    better weight generator

--- src/data/genWeights.py
+++ src/data/genWeights.py
@@ -1,9 +1,29 @@
 import numpy as np
 import pandas as pd
+import random
 
 n = 4
-maxWeight = 0.8
-W = np.random.rand(n,n) * maxWeight
+
+# more random
+# W(i, j) values are random (uniformly distributed)
+# values from 0 to maxWeight = 1 - abs(i - j) / n
+# W = np.zeros((n, n))
+# for i in range(n):
+#     for j in range(n):
+#         maxW = 1 - abs(i - j) / n
+#         W[i, j] = round(maxW * random.random(), 2)
+
+# less random
+# W(i, j) values are random (uniformly distributed)
+# from minWeight = max(maxWeight - randomness, 0)
+# to maxWeight = 1 - abs(i - j) / n
+randomness = 0.3
+W = np.zeros((n, n))
+for i in range(n):
+    for j in range(n):
+        maxW = 1 - abs(i - j) / n
+        W[i, j] = max(maxW - round(random.random() * randomness, 2), 0)
+
 W = np.round(W, 2)
 df = pd.DataFrame(W)
 df.to_csv('weights.csv')
